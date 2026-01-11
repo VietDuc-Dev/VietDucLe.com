fe626ef3ad90ba0a9e9a9b27f646943d33871b5b
vietduc-dev <levietduc.dev@gmail.com>
2026-01-11 19:42:01 +0700

    contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,13 +5,17 @@ import { Textarea } from "@/components/ui/textarea";
 export default function Contact() {
   return (
     <section className="container py-20">
-      <h2 className="text-center text-3xl font-bold mb-12">Have a Project?</h2>
+      <h2 className="text-center text-secondary text-3xl font-bold mb-12">
+        Bạn đang cần <span className="text-primary">Website</span> hoặc{" "}
+        <span className="text-primary">Marketing</span> hiệu quả?
+      </h2>
 
       <form className="max-w-xl mx-auto space-y-4">
-        <Input placeholder="Name" />
-        <Input placeholder="Email" />
-        <Textarea placeholder="Message" />
-        <Button className="w-full">Send Message</Button>
+        <Input placeholder="Tên công ty" />
+        <Input placeholder="Số điện thoại" />
+        <Textarea placeholder="Nội dung tư vấn" />
+        <p>Thông tin được bảo mật - Tư vấn hoàn toàn miễn phí</p>
+        <Button className="w-full">Tư vấn miễn phí ngay</Button>
       </form>
     </section>
   );
